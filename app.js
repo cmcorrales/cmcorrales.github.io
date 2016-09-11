bdb3c91c22098a971d24407bc5afba29394b196e
Cassandra <cmcorrales3@gmail.com>
2016-09-10 23:40:39 -0400

    create projects section

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
 $(document).ready(function(){
-	$('a[href^="#"]').on('click',function (e) {
-	    e.preventDefault();
-
-	    var target = this.hash;
-	    var $target = $(target);
-
-	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
-	    }, 900, 'swing', function () {
-	        window.location.hash = target;
-	    });
-	});
-});
+  $('.pagedown').click(function() {
+      return $(document).height() - this.scrollTop() - this.height();
+  });
+  $('.pageup').click(function() {
+      $('html, body').animate({ scrollTop: 0 });
+  });
+})
